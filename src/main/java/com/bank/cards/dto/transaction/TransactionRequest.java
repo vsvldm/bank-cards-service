91a24085b615cef4bc1a3e050ce5153cf62702cf
Vsevolod <vsevolodmakarsky@yandex.ru>
2026-08-21 01:05:00 +0700

    Remove redundant comment

--- src/main/java/com/bank/cards/dto/transaction/TransactionRequest.java
+++ src/main/java/com/bank/cards/dto/transaction/TransactionRequest.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
-import java.util.UUID; // <-- ДОБАВЛЕНО
+import java.util.UUID;
 
 @Data
 @Builder
